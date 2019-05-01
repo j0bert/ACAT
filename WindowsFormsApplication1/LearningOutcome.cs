7bea5e6cf7c3396e94855e6119256b3af769f1da
sm08333 <sm08333@CEIT2552212X022>
2019-05-01 18:30:57 -0400

    Excel

--- WindowsFormsApplication1/LearningOutcome.cs
+++ WindowsFormsApplication1/LearningOutcome.cs
@@ -20,9 +20,10 @@ namespace WindowsFormsApplication1
         ArrayList ObjectiveNames = new ArrayList(50);
         ArrayList ABETnames = new ArrayList(50);
         ArrayList Assessments = new ArrayList(50);
-        Dictionary<int, string> MOmap = new Dictionary<int, string>();
-        Dictionary<int, string> ABTmap = new Dictionary<int, string>();
-        Dictionary<int, string> Assmap = new Dictionary<int, string>();
+        ArrayList MOmap = new ArrayList(50);
+        ArrayList ABTmap = new ArrayList(50);
+        ArrayList Assmap = new ArrayList(50);
+
 
 
         public LearningOutcome()
@@ -89,15 +90,26 @@ namespace WindowsFormsApplication1
         {
             int sizeOfList;
             int i = 0;
-            MOmap.Add(i, MissionOBBox.ToString());
-            ABTmap.Add(i, ABETLearningBox.ToString());
-            Assmap.Add(i, AssessmentBox.ToString());
-            LoadLearningOutcomesList();
+            foreach (string s in MissionOBBox.CheckedItems)
+            {
+                MOmap.Add(s);
+            }
             i++;
             sizeOfList = outcome.Count;
-            if (outcome[i-1] == outcome[sizeOfList-1])
+
+            if (outcome[i - 1] == outcome[sizeOfList - 1])
             {
-                Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+
+                
+
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
             // creating new WorkBook within Excel application  
             Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
             // creating new Excelsheet in workbook  
@@ -110,18 +122,107 @@ namespace WindowsFormsApplication1
             worksheet = workbook.ActiveSheet;
             // changing the name of active sheet  
             worksheet.Name = "Exported from gridview";
-                foreach (KeyValuePair<int, string> kvp in MOmap)
+            for (int j = 1; j <= 9; j++)
+            {
+                switch (j)
                 {
-                    worksheet.Cells[i + 1, i + 1] = kvp.Value;
+                    case 1:
+                        worksheet.Cells[1, j] = "Learning Outcomes";
+                        worksheet.Cells[1, j].EntireRow.Font.Bold = true;
+                        worksheet.Cells[1, j].EntireRow.Style.WrapText = false;
+
+                        break;
+                    case 2:
+                        worksheet.Cells[1, j] = "Misssion Objective(s)";
+                        worksheet.Cells[1, j].EntireRow.Font.Bold = true;
+                        break;
+                    case 3:
+                        worksheet.Cells[1, j] = "ABET learning Outcomes";
+                        worksheet.Cells[1, j].EntireRow.Font.Bold = true;
+                        break;
+                    case 4:
+                        worksheet.Cells[1, j] = "Evaluation Instrument";
+                        worksheet.Cells[1, j].EntireRow.Font.Bold = true;
+                        break;
+                    case 5:
+                        worksheet.Cells[1, j] = "Avg (%)";
+                        worksheet.Cells[1, j].EntireRow.Font.Bold = true;
+                        break;
+                    case 6:
+                        worksheet.Cells[1, j] = "Standard Deviation";
+                        worksheet.Cells[1, j].EntireRow.Font.Bold = true;
+                        break;
+                    case 7:
+                        worksheet.Cells[1, j] = "Program Outcome";
+                        worksheet.Cells[1, j].EntireRow.Font.Bold = true;
+                        break;
+                    case 8:
+                        worksheet.Cells[1, j] = "Avg";
+                        worksheet.Cells[1, j].EntireRow.Font.Bold = true;
+                        break;
+                    case 9:
+                        worksheet.Cells[1, j] = "Standard Deviation";
+                        worksheet.Cells[1, j].EntireRow.Font.Bold = true;
+                        break;
+                    default:
+                        worksheet.Cells[1, j] = "Opps!";
+                        break;
                 }
-                
+            }
+            for (int j = 0; j <= 2; j++)
+            {
+                switch (j)
+                {
+                    case 0:
+                        worksheet.Cells[j + 17, 1] = "Mission Objectives";
+                        worksheet.Cells[j + 17, 1].EntireRow.Font.Bold = true;
+                        break;
+                    case 1:
+                        worksheet.Cells[j + 24, 1] = "ABET Learning Outcomes";
+                        worksheet.Cells[j + 24, 1].EntireRow.Font.Bold = true;
+                        worksheet.Cells[j + 25, 1] = "The program enables students to achieve, by the time of graduation:";
+                        break;
+                    case 2:
+                        worksheet.Cells[j + 27, 1] = "1. General:";
+                        worksheet.Cells[j + 27, 1].EntireRow.Font.Bold = true;
+                        worksheet.Cells[j + 28, 1] = "(a) An ability to apply knowledge of computing and mathematics appropriate to the discipline";
+                        worksheet.Cells[j + 29, 1] = "(b) An ability to analyze a problem, and identify and define the computing requirements appropriate to its solution;";
+                        worksheet.Cells[j + 30, 1] = "(c) An ability to design, implement and evaluate a computer-based system, process, component, or program to meet desired needs;";
+                        worksheet.Cells[j + 31, 1] = "(d) An ability to function effectively on teams to accomplish a common goal;";
+                        worksheet.Cells[j + 32, 1] = "(e) An understanding of professional, ethical, legal, security, and social issues and responsibilities;";
+                        worksheet.Cells[j + 33, 1] = "(f) An ability to communicate effectively with a range of audiences;";
+                        worksheet.Cells[j + 34, 1] = "(g) An ability to analyze the local and global impact of computing on individuals, organizations and society, including ethical, legal, security and global policy issues;";
+                        worksheet.Cells[j + 35, 1] = "(h) Recognition of the need for, and an ability to engage in, continuing professional development;";
+                        worksheet.Cells[j + 36, 1] = "(i) An ability to use current techniques, skills, and tools necessary for computing practice.";
+                        worksheet.Cells[j + 38, 1] = "2. CS Specific:";
+                        worksheet.Cells[j + 38, 1].EntireRow.Font.Bold = true;
+                        worksheet.Cells[j + 39, 1] = "(a) An ability to apply mathematical foundations, algorithmic principles, and computer science theory in the modeling and design of computer-based systems in a way that demonstrates comprehension of the tradeoffs involved in design choices;";
+                        worksheet.Cells[j + 40, 1] = "(b) An ability to apply design and development principles in the construction of software systems of varying complexity.";
+                        break;
+                    default:
+                        worksheet.Cells[1 + 37, 1] = "Opps!";
+                        break;
+
+                }
+
             }
 
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
+            for (int j = 2; j <= 7; j++)
+            {
+                worksheet.Cells[1, j].WrapText = true;
+            }
+            worksheet.Cells[1, 1].ColumnWidth = 84.14;
+            worksheet.Cells[1, 1].RowHeight = 26.25;
+            // storing Each row and column value in Mission Objectives
+            for (int j = 0; j <= objective.Count - 1; j++)
+            {
+                worksheet.Cells[j + 18, 1] = objective[j].missionObjectiveID.ToString() + ". " + objective[j].missionObjective.ToString();
+            }
+            for (int j = 0; j <= outcome.Count - 1; j++)
+            {
+                worksheet.Cells[j + 2, 1] = outcome[j].outcome.ToString();
+            }
+           
         }
     }
 }
